d9978cba756693dcf9f2e769ae3503db5c2db978
RLC02 <codor417@gmail.com>
2025-02-08 15:40:47 -0300

    v1.1

---
+++ BackEnd/CryAPI/src/test/java/com/cryapi/CryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
